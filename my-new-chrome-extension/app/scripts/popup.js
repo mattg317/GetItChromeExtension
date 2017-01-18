6767f82bc2e2f795e2f2cc2ef3d2839d5a19cc2a
Eedouard Batlle <Eedouard Batlle>
2017-01-18 11:19:15 -0500

    page titles and urls into array(userHistory)

--- my-new-chrome-extension/app/scripts/popup.js
+++ my-new-chrome-extension/app/scripts/popup.js
@@ -7,13 +7,39 @@
 
 // console.log("result " + result)
 
+var userHistory = []
+
 chrome.tabs.getSelected(function(tab){
-        console.log("this page " + tab.url);
+        // console.log("this page " + tab.url);
+        // console.log("this page " + tab.title);
+
+         var newPage = {
+        pageName: tab.title,
+        pageURL: tab.url
+          };
+
+    userHistory.push(newPage)
     }
 );
 
+
+
 chrome.history.search({text: '', maxResults: 1000}, function(data) {
     data.forEach(function(page) {
-        console.log("history " + page.url);
+      
+      var newPage = {
+      	pageName: page.title,
+      	pageURL: page.url
+      }
+
+      userHistory.push(newPage)
     });
 });
+
+console.log(userHistory)
+
+ // var currentURL = window.location.origin;
+
+ //          $.post(currentURL + "/api/eventsCreated", userHistory, function(data){
+ //            console.log(data);
+ //          }); //end POST 
