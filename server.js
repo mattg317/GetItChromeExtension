3549a4e6a49c5ea9581b1348c62bb451dfacea2b
Matt <mattg317@gmail.com>
2017-01-21 10:57:52 -0500

    fixed structure

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ db.once("open", function(){
 });
 
 //using public folder
-app.use(express.static('./public'));
+//app.use(express.static('./public'));
+app.use(express.static(process.cwd() + '/public'));
 
 app.get('/', function(){
 	res.sendFile(__dirname + '/public/index.html');
@@ -43,57 +44,57 @@ app.post("/api", function(req, res){
 
 
 //CHeerio==============================================
-var request = require("request");
-var cheerio = require("cheerio");
+// var request = require("request");
+// var cheerio = require("cheerio");
 
-console.log("\n******************************************\n" +
-            "Grabbing every article headline and link\n" +
-            ":" +
-            "\n******************************************\n");
-var link = "http://www.jerseycityindependent.com/events/"
-
-
-request(link, function(error, response, html){
-
-	//var $ = cheerio.load(html);
-	var $ = cheerio.load("<div class=type-tribe_events> </div> ")
-
-	var results = [];
-
-	// $("h4.headline-link").each(function(i, element){
-
-	// 	var title = $(this).text();
-
-	// 	var link = $(element).parent().attr("href");
-
-	// 	results.push({
-	// 		title: title,
-	// 		link: link
-	// 	});
-
-	// });
-
-	//target coupons 
-
-	// $("div.pods").each(function(i, element){
-	// 	//var title = $(this).children().children('div.pods').children('div.bd').children("div.offer-details");
-
-	// 	//pull titles of comp
-	// 	var title = $(this).children('div').children().children('div').children('div.offer-details').text()
-	// 			console.log(title);
-	// 	// results.push({
-	// 	// })
-	// })
-
-	$('div').each(function(i, element){
-		var title = $(this)
-		//var title = $(this).children().children("h2").text()
-		console.log(title)
-	})
+// console.log("\n******************************************\n" +
+//             "Grabbing every article headline and link\n" +
+//             ":" +
+//             "\n******************************************\n");
+// var link = "http://www.jerseycityindependent.com/events/"
 
 
-	console.log(results);
-})
+// request(link, function(error, response, html){
+
+// 	//var $ = cheerio.load(html);
+// 	var $ = cheerio.load("<div class=type-tribe_events> </div> ")
+
+// 	var results = [];
+
+// 	// $("h4.headline-link").each(function(i, element){
+
+// 	// 	var title = $(this).text();
+
+// 	// 	var link = $(element).parent().attr("href");
+
+// 	// 	results.push({
+// 	// 		title: title,
+// 	// 		link: link
+// 	// 	});
+
+// 	// });
+
+// 	//target coupons 
+
+// 	// $("div.pods").each(function(i, element){
+// 	// 	//var title = $(this).children().children('div.pods').children('div.bd').children("div.offer-details");
+
+// 	// 	//pull titles of comp
+// 	// 	var title = $(this).children('div').children().children('div').children('div.offer-details').text()
+// 	// 			console.log(title);
+// 	// 	// results.push({
+// 	// 	// })
+// 	// })
+
+// 	$('div').each(function(i, element){
+// 		var title = $(this)
+// 		//var title = $(this).children().children("h2").text()
+// 		console.log(title)
+// 	})
+
+
+// 	console.log(results);
+// })
 
 
 //Set server to listen
