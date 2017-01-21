0040ec0b56f766e50f704ed1da8d5ff3aa01a5d4
Matt <mattg317@gmail.com>
2017-01-21 13:32:48 -0500

    post routes

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-//Mongoose=============
+//Models=====================================================
+var Events = require("./server/model.js");
+
+//Mongoose========================================================
 mongoose.connect('mongodb://admin:ruler123@ds117899.mlab.com:17899/heroku_cv93bc03')
 
 var db = mongoose.connection;
@@ -28,76 +31,39 @@ db.once("open", function(){
 	console.log("Mongoose connection successful.");
 });
 
-//using public folder
-//app.use(express.static('./public'));
+//DB add and grab============================
+
+
+//App routes ===========================================================
 app.use(express.static(process.cwd() + '/public'));
 
 app.get('/', function(){
 	res.sendFile(__dirname + '/public/index.html');
 })
 
-app.post("/api", function(req, res){
-	console.log(req.body)
-
+app.get("/api", function(req, res){
+	console.log('api site');
 })
 
+// app.post("/api", function(req, res){
+// 	console.log(req.body)
 
-
-//CHeerio==============================================
-// var request = require("request");
-// var cheerio = require("cheerio");
-
-// console.log("\n******************************************\n" +
-//             "Grabbing every article headline and link\n" +
-//             ":" +
-//             "\n******************************************\n");
-// var link = "http://www.jerseycityindependent.com/events/"
-
-
-// request(link, function(error, response, html){
-
-// 	//var $ = cheerio.load(html);
-// 	var $ = cheerio.load("<div class=type-tribe_events> </div> ")
-
-// 	var results = [];
-
-// 	// $("h4.headline-link").each(function(i, element){
-
-// 	// 	var title = $(this).text();
-
-// 	// 	var link = $(element).parent().attr("href");
-
-// 	// 	results.push({
-// 	// 		title: title,
-// 	// 		link: link
-// 	// 	});
-
-// 	// });
-
-// 	//target coupons 
-
-// 	// $("div.pods").each(function(i, element){
-// 	// 	//var title = $(this).children().children('div.pods').children('div.bd').children("div.offer-details");
-
-// 	// 	//pull titles of comp
-// 	// 	var title = $(this).children('div').children().children('div').children('div.offer-details').text()
-// 	// 			console.log(title);
-// 	// 	// results.push({
-// 	// 	// })
-// 	// })
-
-// 	$('div').each(function(i, element){
-// 		var title = $(this)
-// 		//var title = $(this).children().children("h2").text()
-// 		console.log(title)
-// 	})
-
-
-// 	console.log(results);
 // })
+app.post('/api/posts', function (req, res, next) {
+  
+  var event = new Events({
+    title: req.body.title,
+    url: req.body.url
+  });
+
+  res.send(req.body);
+  console.log(req.body);
+
+});
+//==========================================================
 
 
 //Set server to listen
 app.listen(PORT, function(){
 	console.log("App listening on PORT: "+ PORT);
-})
+});
