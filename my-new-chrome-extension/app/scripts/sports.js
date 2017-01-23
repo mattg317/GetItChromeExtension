6d3bce900629e12d92f8f769561c7d43cd479373
bernerd252 <edouardbatlle@yahoo.com>
2017-01-23 11:33:32 -0800

    sports api

---
+++ my-new-chrome-extension/app/scripts/sports.js
@@ -0,0 +1,17 @@
+var xhr = new XMLHttpRequest();
+
+xhr.open("GET",  "https://api.seatgeek.com/2/events?client_id=NjY4MDExN3wxNDg1MTk5NTUz", false);
+xhr.send();
+
+var result = xhr.responseText;
+
+// for (x=0;x<result.articles)
+console.log("--------------------RESULT-----------------" + result)
+
+var para = document.createElement("P"); 
+
+// document.body.innerHTML = "";
+var t = document.createTextNode(JSON.stringify(result, null, 4));
+
+para.appendChild(t);                                          // Append the text to <p>
+document.getElementById("sports").appendChild(para);
