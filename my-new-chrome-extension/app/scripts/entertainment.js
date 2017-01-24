382f17bbf226b3118816e682f637b2eb3fdb5ef3
bernerd252 <edouardbatlle@yahoo.com>
2017-01-23 17:48:27 -0800

    entertainment

--- my-new-chrome-extension/app/scripts/entertainment.js
+++ my-new-chrome-extension/app/scripts/entertainment.js
@@ -1,21 +1,51 @@
+
+
 var xhr = new XMLHttpRequest();
 
 xhr.open("GET",  "https://getitchrome.herokuapp.com/api/concerts", false);
 xhr.send();
 
 var result = JSON.parse(xhr.responseText);
-ument.getElementById("news").appendChild(a);
+
+// // for (x=0;x<result.articles)
+
+// console.log("--------------------RESULT-----------------" + result.events[1].title)
+// console.log("--------------------RESULT-----------------" + result.events[2].title)
 
 
 
-// for (x=0;x<result.articles)
-console.log("--------------------RESULT-----------------" + result[0].title);
-console.log("--------------------RESULT-----------------" + result[0].url)
 
-var para = document.createElement("P"); 
+// var para = document.createElement("P"); 
 
-// document.body.innerHTML = "";
-var t = document.createTextNode(JSON.stringify(result, null, 4));
+// // document.body.innerHTML = "";
+// //var t = document.createTextNode(JSON.stringify(result, null, 4));
+// var t = document.createTextNode(JSON.stringify(result.events[1].title, null, 4));
 
-para.appendChild(t);                                          // Append the text to <p>
-document.getElementById("entertainment").appendChild(para);
+// para.appendChild(t);                                          // Append the text to <p>
+// document.getElementById("sports").appendChild(para);
+
+
+for(var i=0; i<5; i++){
+	console.log("--------------------RESULT-----------------" + result[i].title);
+	console.log("--------------------RESULT-----------------" + result[i].url);
+
+	var para = document.createElement("P"); 
+
+		// document.body.innerHTML = "";
+		//var t = document.createTextNode(JSON.stringify(result, null, 4));
+		var t = document.createTextNode(JSON.stringify(result[i].title, null, 4));
+		var u = document.createTextNode(JSON.stringify(result[i].url, null, 4));
+		var div = document.createElement('div');
+		var a = document.createElement('a');
+			a.appendChild(u);
+			a.href = u;
+		//para.appendChild(t);
+		//para.appendChild(u);    
+		div.appendChild(t)
+		div.appendChild(a)
+		 
+	                                 // Append the text to <p>
+		document.getElementById("entertainment").appendChild(div);
+};
+
+
