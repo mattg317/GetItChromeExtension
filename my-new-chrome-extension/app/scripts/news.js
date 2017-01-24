0ab9819517f9f0617d90f403f78e44f856170fec
bernerd252 <edouardbatlle@yahoo.com>
2017-01-23 17:38:57 -0800

    news

--- my-new-chrome-extension/app/scripts/news.js
+++ my-new-chrome-extension/app/scripts/news.js
@@ -4,56 +4,29 @@ xhr.open("GET",  "https://newsapi.org/v1/articles?source=google-news&sortBy=top&
 xhr.send();
 
 var result = JSON.parse(xhr.responseText);
-var parsedLink = []
-// for (x=0;x<result.articles)
-console.log("--------------------RESULT-----------------" + result.articles[0].title)
-console.log("--------------------RESULT-----------------" + result.articles[0].url)
-
-for (x=0;x<result.articles.length;x++){
-
-var a = document.createElement('a');
-// document.body.innerHTML = "";
-var linkText = document.createTextNode(result.articles[0].title);
-var linkUrl = document.createTextNode(result.articles[0].url)
-console.log(linkUrl)
-a.appendChild(linkText);
-
-a.title = linkText;
-a.href = parsedLink[0];
-
-
-// para.appendChild(t);                                          // Append the text to <p>
-document.getElementById("news").appendChild(a);
-}
-
-// var a = document.createElement('a');
-// var linkText = document.createTextNode("my title text");
-// a.appendChild(linkText);
-// a.title = "my title text";
-// a.href = "http://example.com";
-// document.body.appendChild(a);
-
-// function parseURL(url) {
-//     var parser = document.createElement('a'),
-//         searchObject = {},
-//         queries, split, i;
-//     // Let the browser do the work
-//     parser.href = linkUrl;
-//     // Convert query string to object
-//     queries = parser.search.replace(/^\?/, '').split('&');
-//     for( i = 0; i < queries.length; i++ ) {
-//         split = queries[i].split('=');
-//         searchObject[split[0]] = split[1];
-//     }
-//     return {
-//         protocol: parser.protocol,
-//         host: parser.host,
-//         hostname: parser.hostname,
-//         port: parser.port,
-//         pathname: parser.pathname,
-//         search: parser.search,
-//         searchObject: searchObject,
-//         hash: parser.hash
-//     };
-//     console.log(parser)
+ument.getElementById("news").appendChild(a);
 // }
+
+for(var i=0; i<5; i++){
+	console.log("--------------------RESULT-----------------" + result.articles[i].title);
+	console.log("--------------------RESULT-----------------" + result.articles[i].url);
+
+	var para = document.createElement("P"); 
+
+		// document.body.innerHTML = "";
+		//var t = document.createTextNode(JSON.stringify(result, null, 4));
+		var t = document.createTextNode(JSON.stringify(result.articles[i].title, null, 4));
+		var u = document.createTextNode(JSON.stringify(result.articles[i].url, null, 4));
+		var div = document.createElement('div');
+		var a = document.createElement('a');
+			a.appendChild(u);
+			a.href = u;
+		//para.appendChild(t);
+		//para.appendChild(u);    
+		div.appendChild(t)
+		div.appendChild(a)
+		 
+	                                 // Append the text to <p>
+		document.getElementById("news").appendChild(div);
+};
+
