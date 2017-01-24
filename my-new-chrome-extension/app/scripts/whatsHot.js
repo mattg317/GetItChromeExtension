d234bc3932353ae070530364bd2119db60539171
Matt <mattg317@gmail.com>
2017-01-23 20:57:24 -0500

    fixed whatsHot

--- my-new-chrome-extension/app/scripts/whatsHot.js
+++ my-new-chrome-extension/app/scripts/whatsHot.js
@@ -3,15 +3,39 @@ var xhr = new XMLHttpRequest();
 xhr.open("GET",  "https://getitchrome.herokuapp.com/api/events", false);
 xhr.send();
 
-var result = xhr.responseText;
+var result = JSON.parse(xhr.responseText);
 
-// for (x=0;x<result.articles)
-console.log("--------------------RESULT-----------------" + result.title)
+// // for (x=0;x<result.articles)
+// console.log("--------------------RESULT-----------------" + result[.title)
 
-var para = document.createElement("P"); 
+// var para = document.createElement("P"); 
 
-// document.body.innerHTML = "";
-var t = document.createTextNode(JSON.stringify(result, null, 4));
+// // document.body.innerHTML = "";
+// var t = document.createTextNode(JSON.stringify(result, null, 4));
 
-para.appendChild(t);                                          // Append the text to <p>
-document.getElementById("whatsHot").appendChild(para);
+// para.appendChild(t);                                          // Append the text to <p>
+// document.getElementById("whatsHot").appendChild(para);
+
+
+for(var i=0; i<5; i++){
+	console.log("--------------------RESULT-----------------" + result[i].title);
+	console.log("--------------------RESULT-----------------" + result[i].url);
+
+	var para = document.createElement("P"); 
+
+		// document.body.innerHTML = "";
+		//var t = document.createTextNode(JSON.stringify(result, null, 4));
+		var t = document.createTextNode(JSON.stringify(result[i].title, null, 4));
+		var u = document.createTextNode(JSON.stringify(result[i].url, null, 4));
+		var div = document.createElement('div');
+		var a = document.createElement('a');
+			a.appendChild(u);
+			a.href = u;
+		//para.appendChild(t);
+		//para.appendChild(u);    
+		div.appendChild(t)
+		div.appendChild(a)
+		 
+	                                 // Append the text to <p>
+		document.getElementById("whatsHot").appendChild(div);
+};
